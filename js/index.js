9123df4fba4762678323db1cc570895b5439a2bb
Aadit Gupta <spacegiant21@gmail.com>
2020-05-30 15:07:12 -0700

    add website files

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var header = document.getElementById("myHeader");
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
