97187ecfee56b236ffa8403e0f14c5f893429b4f
Raadwan Masum <piraadwan@gmail.com>
2020-08-18 16:38:20 -0700

    prettier

--- js/index.js
+++ js/index.js
@@ -29,35 +29,36 @@ $(document).ready(function () {
 });
 
 $(window).on("load", function () {
-  document.getElementById("myload").src = "https://raw.githubusercontent.com/RGBHackers/RGBHackers.github.io/master/img/loader.svg?" + Math.random();
+  document.getElementById("myload").src =
+    "https://raw.githubusercontent.com/RGBHackers/RGBHackers.github.io/master/img/loader.svg?" +
+    Math.random();
   var opacity = 0;
   var opacity2 = 1;
   var speed = 0.05;
   setTimeout(setup, 4000);
-  function setup () {
+  function setup() {
     //document.getElementById('loader-wrapper').classList.toggle("fade");
     //document.getElementById('loader-wrapper').style.opacity = 0.9
     //document.getElementById('change-overflow').style.overflow = "auto"
-    incOpacity2()
+    incOpacity2();
   }
-  function incOpacity () {
+  function incOpacity() {
     opacity += speed;
-    document.getElementById('change-overflow').style.opacity = opacity;
+    document.getElementById("change-overflow").style.opacity = opacity;
     if (opacity < 1) {
-      setTimeout(incOpacity,100)
+      setTimeout(incOpacity, 100);
     }
   }
-  function incOpacity2 () {
+  function incOpacity2() {
     opacity2 = opacity2 - speed;
     if (opacity2 >= 0) {
-      console.log(opacity2)
-      document.getElementById('loader-wrapper').style.opacity = opacity2
-      setTimeout(incOpacity2,30)
-    }
-    else {
-      document.getElementById('loader-wrapper').style.zIndex = "-9999999999"
-      document.getElementById('change-overflow').style.overflow = "auto"
-      document.getElementById('loader-wrapper').style.opacity = 0
+      console.log(opacity2);
+      document.getElementById("loader-wrapper").style.opacity = opacity2;
+      setTimeout(incOpacity2, 30);
+    } else {
+      document.getElementById("loader-wrapper").style.zIndex = "-9999999999";
+      document.getElementById("change-overflow").style.overflow = "auto";
+      document.getElementById("loader-wrapper").style.opacity = 0;
     }
   }
 });
